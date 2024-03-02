a78016f95dd51682969854ef8a906b8c4f3f11d8
Justo Diaz <justodiaz9@gmail.com>
2024-03-01 20:32:16 -0600

    Updates to readme, pom, and test

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Simple Java app that queries `forismatic.com/api/1.0/` to get a random quote and author. 
-Takes in a command line argument that can be either "English" or "Russian" to choose the language of the quoute.
+Takes in a command line argument that can be either "English" or "Russian" to choose the language of the quote.
 If no argument is passed or an invalid `String` is given, it defaults to "English"
+
+Project requires Maven and Java 21 to be installed. To run, it may be easier to use an IDE and import repo as a 
+Maven project. The `main` method is in `org.example.Quotely`.
+
+`pom.xml` file is configured to package all dependencies into 1 jar. 
+So it may be possible to run from command line with: 
+
+`mvn package -f pom.xml`
+
+`java -jar Quotely-1.0-SNAPSHOT-jar-with-dependencies.jar`
+
+Optional params can go at the end e.g.
+
+`java -jar Quotely-1.0-SNAPSHOT-jar-with-dependencies.jar Russian`
--- src/test/java/org/example/ForismaticAPITest.java
+++ src/test/java/org/example/ForismaticAPITest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ForismaticAPITest {
-  private ForismaticAPI forismaticAPI;
+  private final ForismaticAPI forismaticAPI;
 
   public ForismaticAPITest() {
     var mockRequester = new HTTPJsonRequestClient() {
       @Override
       public <T> Optional<T> send(HttpRequest httpRequest, Class<T> type) {
         if (type.equals(Quote.class)) {
-          return Optional.<T>of((T) new Quote("Java 21 is great", "Java Beans"));
+          return Optional.of((T) new Quote("Java 21 is great", "Java Beans"));
         }
         return Optional.empty();
       }
@@ -32,5 +32,11 @@ class ForismaticAPITest {
     assertTrue(actual.isPresent());
     assertEquals("Java 21 is great", actual.get().quoteText());
     assertEquals("Java Beans", actual.get().quoteAuthor());
+  }
+
+  @Test
+  void getQuote_UnknownLanguage_GetsEmptyResponse() {
+    var actual = forismaticAPI.getQuote(Language.UNKNOWN);
+    assertTrue(actual.isEmpty());
   }
 }
